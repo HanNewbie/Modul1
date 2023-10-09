fb4941569078b8aee4d104a23c17419365746735
HanNewbie <farhannugroho04@gmail.com>
2023-10-09 21:49:47 +0700

    Modul1

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+// GUIDED 1
+public class Main {
+    public static void main(String args[])
+    {
+        int jumlah_looping=5;
+        int bil1=5;
+        int bil2=2;
+        DemoObjectOriented Demo = new DemoObjectOriented ();
+        Demo.helloWorld(jumlah_looping);
+        System.out.print("\n-Pemanggilan Fungsi Tambah-\n");
+        System.out.print("	Hasil	Operasi	Tambah "+Demo.tambah(bil1,bil2)+"\n");
+
+    }
+}
+
+
+/*//GUIDED 2
+public class Main {
+    public static void main(String[] args) {
+        Welcome W = new Welcome();
+        W.display();
+    }
+}
+*/
+
+
+/* //GUIDED 3
+public class Main {
+    public static void main(String args[]){
+        Mahasiswa M= new Mahasiswa(); //Pembentukan Objek
+        M.inpuData();
+        M.display();
+    }
+}
+ */
